8c47357427c756460a2d07f727e7775d85fd1334
farisamamnoon <farismamnoon1@gmail.com>
2026-02-03 06:34:06 +0530

    formatter for ts

--- init.lua
+++ init.lua
@@ -718,7 +718,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -809,7 +809,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
       },
     },
   },
